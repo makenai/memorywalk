10d79d5141a91db599a42310b019d320efb464b9
Pawel Szymczykowski <makenai@gmail.com>
2013-06-18 17:51:49 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MemoryWalk::Application.config.secret_token = 'eae575c7421513567d99fccc9e5fe930290545db84319ca0a52e196587cb861781905cf77bbb77aedab63f5631fb43144ed747886f464a0a7ae5a69ee96fdd55'

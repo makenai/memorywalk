b505d0b27558155c8429d5fc9f8a8b6eaa717d52
Pawel Szymczykowski <makenai@gmail.com>
2013-06-22 16:41:08 -0700

    About page

--- config/routes.rb
+++ config/routes.rb
@@ -50,6 +50,8 @@ MemoryWalk::Application.routes.draw do
   #     resources :products
   #   end
 
+  match 'about' => 'walks#about'
+
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'walks#index'
